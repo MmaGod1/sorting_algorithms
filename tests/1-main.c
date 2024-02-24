a35bd6df23bfe052bff7e08e92888e0ff942afa3
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-02-25 00:45:10 +0100

    Update 1-main.c

--- tests/1-main.c
+++ tests/1-main.c
@@ -6,20 +6,29 @@
  * @n: Value for the new node
  * Return: A pointer to the newly created node
  */
-listint_t *create_node(int n)
+listint_t *create_node(const int *array, size_t size)
 {
-	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *list;
+	listint_t *node;
+	int *tmp;
 
-	if (new_node == NULL)
+	list = NULL;
+	while (size--)
 	{
-		fprintf(stderr, "Memory allocation failed\n");
-		return (NULL);
+		node = malloc(sizeof(*node));
+		if (!node)
+			return (NULL);
+		tmp = (int *)&node->n;
+		*tmp = array[size];
+		node->next = list;
+		node->prev = NULL;
+		list = node;
+		if (list->next)
+			list->next->prev = list;
 	}
-	(int *)&new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
-	return (new_node);
+	return (list);
 }
+
 
 /**
  * free_list - Frees a doubly linked list
@@ -44,30 +53,17 @@ void free_list(listint_t *head)
  */
 int main(void)
 {
-	listint_t *head = NULL;
+    listint_t *list;
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    size_t n = sizeof(array) / sizeof(array[0]);
 
-	/* Create a doubly linked list */
-	listint_t *node1 = create_node(4);
-	listint_t *node2 = create_node(2);
-	listint_t *node3 = create_node(1);
-	listint_t *node4 = create_node(3);
-
-	head = node1;
-	node1->next = node2;
-	node2->prev = node1;
-	node2->next = node3;
-	node3->prev = node2;
-	node3->next = node4;
-	node4->prev = node3;
-	printf("Original list: ");
-	print_list(head);
-
-	/* Sort the list using insertion sort */
-	insertion_sort_list(&head);
-	printf("Sorted list: ");
-	print_list(head);
-
-	/* Free the memory allocated for the list */
-	free_list(head);
-	return (0);
+    list = create_listint(array, n);
+    if (!list)
+        return (1);
+    print_list(list);
+    printf("\n");
+    insertion_sort_list(&list);
+    printf("\n");
+    print_list(list);
+    return (0);
 }
