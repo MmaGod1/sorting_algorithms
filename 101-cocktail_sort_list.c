4948393fbc02f44b3300872f87c7e1e2426e9510
root <gmmaduchukwu@gmail.com>
2025-09-16 04:40:22 -0700

    Update

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,99 +1,99 @@
+
 #include "sort.h"
 
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **ptr);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **ptr);
+void cocktail_sort_list(listint_t **list);
+
 /**
- * swap_nodes - Swap two adjacent nodes in a doubly linked list
- * @list: pointer to the head of the list
- * @left: pointer to the first node
- * @right: pointer to the second node
+ * swap_node_ahead - Swap a node in a listint_t doubly-linked list
+ *                   list of integers with the node ahead of it.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ * @tail: A pointer to the tail of the doubly-linked list.
+ * @ptr: A pointer to the current swapping node of the cocktail ptr algo.
  */
-void swap_nodes(listint_t **list, listint_t *left, listint_t *right)
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **ptr)
 {
-	if (left->prev)
-		left->prev->next = right;
+	listint_t *tmp = (*ptr)->next;
+
+	if ((*ptr)->prev != NULL)
+		(*ptr)->prev->next = tmp;
 	else
-		*list = right;
-	if (right->next)
-		right->next->prev = left;
-	right->prev = left->prev;
-	left->next = right->next;
-	right->next = left;
-	left->prev = right;
+		*list = tmp;
+	tmp->prev = (*ptr)->prev;
+	(*ptr)->next = tmp->next;
+	if (tmp->next != NULL)
+		tmp->next->prev = *ptr;
+	else
+		*tail = *ptr;
+	(*ptr)->prev = tmp;
+	tmp->next = *ptr;
+	*ptr = tmp;
 }
 
 /**
- * forward_pass - Perform forward pass of cocktail sort
- * @list: pointer to the head of the list
- * @start: starting node for the pass
- * @end: ending node for the pass
- * Return: 1 if swap occurred, 0 otherwise
+ * swap_node_behind - Swap a node in a listint_t doubly-linked
+ *                    list of integers with the node behind it.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ * @tail: A pointer to the tail of the doubly-linked list.
+ * @ptr: A pointer to the current swapping node of the cocktail ptr algo.
  */
-int forward_pass(listint_t **list, listint_t *start, listint_t *end)
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **ptr)
 {
-	int swapped = 0;
-	listint_t *current = start;
+	listint_t *tmp = (*ptr)->prev;
 
-	while (current && current->next != end)
-	{
-		if (current->n > current->next->n)
-		{
-			swap_nodes(list, current, current->next);
-			swapped = 1;
-			print_list(*list);
-		}
-		else
-			current = current->next;
-	}
-	return (swapped);
+	if ((*ptr)->next != NULL)
+		(*ptr)->next->prev = tmp;
+	else
+		*tail = tmp;
+	tmp->next = (*ptr)->next;
+	(*ptr)->prev = tmp->prev;
+	if (tmp->prev != NULL)
+		tmp->prev->next = *ptr;
+	else
+		*list = *ptr;
+	(*ptr)->next = tmp;
+	tmp->prev = *ptr;
+	*ptr = tmp;
 }
 
 /**
- * backward_pass - Perform backward pass of cocktail sort
- * @list: pointer to the head of the list
- * @start: starting node for the pass
- * @end: ending node for the pass
- * Return: 1 if swap occurred, 0 otherwise
- */
-int backward_pass(listint_t **list, listint_t *start, listint_t *end)
-{
-	int swapped = 0;
-	listint_t *current = end;
-
-	while (current && current->prev && current != start)
-	{
-		if (current->prev->n > current->n)
-		{
-			swap_nodes(list, current->prev, current);
-			swapped = 1;
-			print_list(*list);
-		}
-		else
-			current = current->prev;
-	}
-	return (swapped);
-}
-
-/**
- * cocktail_sort_list - Sort a doubly linked list using cocktail sort
- * @list: pointer to the head of the list
+ * cocktail_sort_list - Sort a listint_t doubly-linked list of integers in
+ *                      ascending order using the cocktail ptr algorithm.
+ * @list: A pointer to the head of a listint_t doubly-linked list.
  */
 void cocktail_sort_list(listint_t **list)
 {
-	int forward_swapped, backward_swapped;
-	listint_t *start, *end, *current;
+	listint_t *tail, *ptr;
+	bool ptr_not_stirred = false;
 
-	if (!list || !*list || !(*list)->next)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	start = *list;
-	end = NULL;
-	do {
-		forward_swapped = forward_pass(list, start, end);
-		current = start;
-		while (current && current->next != end)
-			current = current->next;
-		end = current;
-		if (!forward_swapped)
-			break;
-		backward_swapped = backward_pass(list, start, end);
-		start = *list;
-	} while (forward_swapped || backward_swapped);
+
+	for (tail = *list; tail->next != NULL;)
+		tail = tail->next;
+
+	while (ptr_not_stirred == false)
+	{
+		ptr_not_stirred = true;
+		for (ptr = *list; ptr != tail; ptr = ptr->next)
+		{
+			if (ptr->n > ptr->next->n)
+			{
+				swap_node_ahead(list, &tail, &ptr);
+				print_list((const listint_t *)*list);
+				ptr_not_stirred = false;
+			}
+		}
+		for (ptr = ptr->prev; ptr != *list;
+				ptr = ptr->prev)
+		{
+			if (ptr->n < ptr->prev->n)
+			{
+				swap_node_behind(list, &tail, &ptr);
+				print_list((const listint_t *)*list);
+				ptr_not_stirred = false;
+			}
+		}
+	}
 }
