40b5fc486e712ed9ce03a3079426644db0130725
root <gmmaduchukwu@gmail.com>
2025-09-18 11:15:48 -0700

    Remove swap_ints prototwqype

--- sort.h
+++ sort.h
@@ -65,6 +65,5 @@ void heap_sort(int *array, size_t size);
 int get_max(int *array, size_t size);
 void counting_sort_radix(int *array, size_t size, int exp);
 void radix_sort(int *array, size_t size);
-void swap_ints(int *a, int *b);
 
 #endif /* SORT_H */
